97c6a4ad49a1ea42899c5162e9c432fd6a1d090c
ElHurta <juan.david.hurtado08@gmail.com>
2023-09-11 23:20:52 -0500

    initial commmit

---
+++ src/app/components/postComponent.tsx
@@ -0,0 +1,5 @@
+export default function postComponent () {
+  return (
+    <div>post</div>
+  )
+}
---
+++ src/app/components/postsList.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import postComponent from './postComponent'
+
+export default function postsList () {
+  return (
+    <div>postsList</div>
+  )
+}
